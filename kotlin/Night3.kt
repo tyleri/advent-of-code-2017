eaf7531247ec370d7c9abbce69b5d5d4fb728098
Tyler Ishikawa <tyi3@cornell.edu>
2017-12-08 01:43:43 -0500

    add kotlin code for nights 1-6

---
+++ kotlin/Night3.kt
@@ -0,0 +1,22 @@
+fun main(args : Array<String>) {
+    var input : Int = 312051
+
+    println("Result: ${part1(input)}")
+}
+
+fun part1(input : Int) : Int {
+    if (input == 1) {
+        return 0
+    }
+
+    var i = 1
+    while (i*i < input) {
+        i += 2
+    }
+
+    val layers_deep : Int = i / 2
+
+    val place_on_side : Int = ( input - (i - 2) * (i - 2) - 1 ) % (i - 1)
+
+    return layers_deep + Math.abs(place_on_side - (layers_deep - 1))
+}
---
+++ kotlin/Night6.kt
@@ -0,0 +1,88 @@
+fun main(args : Array<String>) {
+    val input = readLine()
+
+    if (input != null) {
+        println("Part 1: ${part1(input)}")
+        println("Part 2: ${part2(input)}")
+    }
+}
+
+fun intArrToStr(arr : Array<Int>) : String = arr.fold( "", {acc, elt -> acc + elt} )
+
+fun part1(input : String) : Int? {
+    var banks : Array<Int> = input.split("\t").map {it.toInt()}.toTypedArray()
+    val set = HashSet<String>()
+    var times = 0
+
+    while (!set.contains(intArrToStr(banks))) {
+        set.add(intArrToStr(banks))
+
+        // get max
+        var max = banks[0]
+        var maxIndex = 0
+
+        banks.forEachIndexed { index, value ->
+            if (value > max) {
+                max = value
+                maxIndex = index
+            }
+        }
+
+        banks[maxIndex] = 0
+
+        // distribute
+        var toDistribute = max
+        var distrIndex = maxIndex
+        while (toDistribute > 0) {
+            distrIndex = (distrIndex + 1) % banks.size
+            banks[distrIndex]++
+            toDistribute--
+        }
+
+        times++
+    }
+
+    return times
+}
+
+fun part2(input : String) : Int? {
+    var banks : Array<Int> = input.split("\t").map {it.toInt()}.toTypedArray()
+    var step = 0
+
+    val map = HashMap<String, Int>()
+
+    while (!map.containsKey(intArrToStr(banks))) {
+        map.put(intArrToStr(banks), step)
+
+        // get max
+        var max = banks[0]
+        var maxIndex = 0
+
+        banks.forEachIndexed { index, value ->
+            if (value > max) {
+                max = value
+                maxIndex = index
+            }
+        }
+
+        banks[maxIndex] = 0
+
+        // distribute
+        var toDistribute = max
+        var distrIndex = maxIndex
+        while (toDistribute > 0) {
+            distrIndex = (distrIndex + 1) % banks.size
+            banks[distrIndex]++
+            toDistribute--
+        }
+
+        step++
+    }
+
+    val mappedVal = map.get(intArrToStr(banks))
+    if (mappedVal != null) {
+        return step - mappedVal
+    } else {
+        return null
+    }
+}
