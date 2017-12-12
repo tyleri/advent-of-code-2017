9a220d84fd9f48adec63291dc12be51847596050
Tyler Ishikawa <tyi3@cornell.edu>
2017-12-12 00:14:43 -0500

    solve part 2 of night 9 problem

--- kotlin/Night9.kt
+++ kotlin/Night9.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) {
     val input = readLine() ?: throw IllegalArgumentException("No input")
 
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
 
 sealed class Bunch
@@ -61,4 +62,49 @@ fun part1(input: String) : Int {
     }
 
     return totalScore
+}
+
+fun part2(input: String) : Int {
+    var queue = LinkedList<Bunch>()
+    var garbageChars = 0
+
+    fun parseChar(char: Char) : Token = when (char) {
+        '{' -> Token.OPEN_GROUP
+        '}' -> Token.CLOSE_GROUP
+        '<' -> Token.OPEN_GARBAGE
+        '>' -> Token.CLOSE_GARBAGE
+        '!' -> Token.CANCEL
+        else -> Token.OTHER
+    }
+
+    fun processChar(char: Char) {
+        val topElt = queue.peek()
+        when (topElt) {
+            Cancel  -> queue.pop()
+            Garbage -> when (parseChar(char)) {
+                Token.CANCEL        -> queue.push( Cancel )
+                Token.CLOSE_GARBAGE -> queue.pop()
+                else                -> garbageChars++
+            }
+            is Group -> when (parseChar(char)) {
+                Token.CANCEL        -> queue.push( Cancel )
+                Token.CLOSE_GROUP   -> queue.pop()
+                Token.OPEN_GROUP    -> queue.push( Group(topElt.score + 1) )
+                Token.OPEN_GARBAGE  -> queue.push( Garbage )
+                else -> {}
+            }
+            null -> when (parseChar(char)) {
+                Token.CANCEL        -> queue.push( Cancel )
+                Token.OPEN_GROUP    -> queue.push( Group(1) )
+                Token.OPEN_GARBAGE  -> queue.push( Garbage )
+                else -> {}
+            }
+        }
+    }
+
+    for (char in input) {
+        processChar(char)
+    }
+
+    return garbageChars
 }
