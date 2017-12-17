057588611b6548a149c382cc89c3b587d0bf0500
Tyler Ishikawa <tyi3@cornell.edu>
2017-12-17 15:03:35 -0500

    add solution for night 17 part 2 in kotlin

--- kotlin/Night17.kt
+++ kotlin/Night17.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     val input = readLine()?.toInt() ?: throw IllegalArgumentException("No input")
 
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
 
 fun part1(input: Int) : Int {
@@ -15,4 +16,19 @@ fun part1(input: Int) : Int {
     }
 
     return buffer.get(currPos + 1)
+}
+
+fun part2(input: Int) : Int {
+    var currPos = 0
+    var afterZero = 0
+
+    for (i in 1..50_000_000) {
+        currPos = (currPos + input) % i + 1
+
+        if (currPos == 1) {
+            afterZero = i
+        }
+    }
+
+    return afterZero
 }
