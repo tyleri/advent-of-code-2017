e726acb53aba906219652a152c8c9c4ea3a1e18d
Tyler Ishikawa <tyi3@cornell.edu>
2017-12-17 15:32:47 -0500

    refactor night 12 code to use helper UnionFind class

--- kotlin/Night12.kt
+++ kotlin/Night12.kt
@@ -1,4 +1,5 @@
 import java.util.LinkedList
+import helper.UnionFind
 
 fun main(args: Array<String>) {
     var input = LinkedList<String>()
@@ -18,46 +19,29 @@ fun main(args: Array<String>) {
     println("Part 2: ${part2(input)}")
 }
 
-fun ufGetGroup(groups: Array<Int>, elt: Int) : Int {
-    var currElt = elt
-    while (groups[currElt] != currElt) {
-        currElt = groups[currElt]
-    }
-
-    return currElt
-}
-
 fun part1(input: List<String>) : Int {
-    val unionFind = Array<Int>( size=input.size, init={it} )
+    val uf = UnionFind(0 until input.size)
     input.forEach {
         val lineVals = it.split(",", "<->").map(String::trim).map(String::toInt)
 
-        val topRoot = ufGetGroup(unionFind, lineVals.first())
         lineVals.drop(1).forEach {
-            val oldRoot = ufGetGroup(unionFind, it)
-            unionFind[oldRoot] = topRoot
+            uf.union(lineVals.first(), it)
         }
     }
 
-    val zeroGroup = ufGetGroup(unionFind, 0)
-    return unionFind.indices.count { zeroGroup == ufGetGroup(unionFind, it) }
+    val zeroGroup = uf.find(0)
+    return (0 until input.size).count { zeroGroup == uf.find(it) }
 }
 
 fun part2(input: List<String>) : Int {
-    val unionFind = Array<Int>( size=input.size, init={it} )
+    val uf = UnionFind(0 until input.size)
     input.forEach {
         val lineVals = it.split(",", "<->").map(String::trim).map(String::toInt)
 
-        val topRoot = ufGetGroup(unionFind, lineVals.first())
         lineVals.drop(1).forEach {
-            val oldRoot = ufGetGroup(unionFind, it)
-            unionFind[oldRoot] = topRoot
+            uf.union(lineVals.first(), it)
         }
     }
 
-    return unionFind
-        .indices
-        .map {ufGetGroup(unionFind, it)}
-        .distinct()
-        .size
+    return uf.mapping.values.map {uf.find(it)}.distinct().size
 }
