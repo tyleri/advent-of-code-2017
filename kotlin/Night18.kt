26f5cc07f8ef680bf633490f482f9317e03781ad
Tyler Ishikawa <tyi3@cornell.edu>
2017-12-19 00:49:56 -0500

    add solution for night 18 part 2 in kotlin

--- kotlin/Night18.kt
+++ kotlin/Night18.kt
@@ -1,3 +1,5 @@
+import java.util.LinkedList
+
 fun main(args: Array<String>) {
     val input = mutableListOf<String>()
 
@@ -14,10 +16,11 @@ fun main(args: Array<String>) {
     }
 
     println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
 
 sealed class Instruction
-data class Snd(val register: Char) : Instruction()
+data class Snd(val amount: Long) : Instruction()
 data class Set(val register: Char, val amount: Long) : Instruction()
 data class Add(val register: Char, val amount: Long) : Instruction()
 data class Mul(val register: Char, val amount: Long) : Instruction()
@@ -29,7 +32,15 @@ fun parseInstruction(line: String, registers: Map<Char, Long>) : Instruction {
     val splitLine = line.split(" ")
 
     return when (splitLine[0]) {
-        "snd" -> Snd(splitLine[1][0])
+        "snd" -> {
+            Snd(
+                if (splitLine[1].length == 1 && splitLine[1][0] in 'a'..'z') {
+                    registers.getOrDefault(splitLine[1][0], 0L)
+                } else {
+                    splitLine[1].toLong()
+                }
+            )
+        }
         "set" -> {
             Set(
                 splitLine[1][0],
@@ -88,7 +99,7 @@ fun part1(input: List<String>) : Long {
     while (i <= input.lastIndex) {
         val curr = parseInstruction(input[i], registers)
         when (curr) {
-            is Snd -> lastSound = registers.getOrDefault(curr.register, 0L)
+            is Snd -> lastSound = curr.amount
             is Set -> registers.put(curr.register, curr.amount)
             is Add -> registers.put(curr.register, registers.getOrDefault(curr.register, 0) + curr.amount)
             is Mul -> registers.put(curr.register, registers.getOrDefault(curr.register, 0) * curr.amount)
@@ -99,4 +110,53 @@ fun part1(input: List<String>) : Long {
         i++
     }
     return 0 // should never get here
+}
+
+data class ProgState(
+    var pc: Int,
+    var numSent: Int,
+    val sndQueue: LinkedList<Long>,
+    val rcvQueue: LinkedList<Long>,
+    val registers: MutableMap<Char, Long>
+)
+
+fun part2(input: List<String>) : Int {
+    val sndQueues = arrayOf(LinkedList<Long>(), LinkedList<Long>())
+    val states = arrayOf(
+        ProgState(0, 0, sndQueues[0], sndQueues[1], mutableMapOf('p' to 0L) ),
+        ProgState(0, 0, sndQueues[1], sndQueues[0], mutableMapOf('p' to 1L) )
+    )
+
+    fun runProgram(progNum: Int) : Int {
+        val state = states[progNum]
+        var instructionsExec = 0
+        while (state.pc <= input.lastIndex) {
+            val curr = parseInstruction(input[state.pc], state.registers)
+            when (curr) {
+                is Snd -> {
+                    state.sndQueue.offer( curr.amount )
+                    state.numSent++
+                }
+                is Rcv -> {
+                    if (state.rcvQueue.isEmpty()) {
+                        return instructionsExec
+                    }
+                    val receivedVal = state.rcvQueue.poll()
+                    state.registers.put(curr.register, receivedVal)
+                }
+                is Set -> state.registers.put(curr.register, curr.amount)
+                is Add -> state.registers.put(curr.register, state.registers.getOrDefault(curr.register, 0) + curr.amount)
+                is Mul -> state.registers.put(curr.register, state.registers.getOrDefault(curr.register, 0) * curr.amount)
+                is Mod -> state.registers.put(curr.register, state.registers.getOrDefault(curr.register, 0) % curr.amount)
+                is Jgz -> if (curr.cmp > 0) state.pc += curr.amount.toInt() - 1
+            }
+            state.pc++
+            instructionsExec++
+        }
+        return instructionsExec
+    }
+
+    while (runProgram(0) > 0 && runProgram(1) > 0) {}
+
+    return states[1].numSent
 }
